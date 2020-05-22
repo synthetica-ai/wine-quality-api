fdf464be1d55bd02f87d40c8fe042ebcbabb02a4
stefanosKarantin <karantinakis.s@gmail.com>
2020-05-22 15:51:56 +0300

    python exercises

--- README.md
+++ README.md
@@ -4,22 +4,28 @@ This is a repository for training purposes
 ## Exercise
 * Create branch from master with the format ```api-init-<YOUR_NAME>```
 * Install Docker and Docker-Compose following the link below
-    * https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
-    * https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-18-04
+    * linux (ubuntu):
+        * https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
+        * https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-18-04
+    * windows:
+        * https://docs.docker.com/docker-for-windows/install/
 * Run from inside the project folder ```docker-compose up --build```
 * Create an api with the flask microframework with the entrypoint being at ```api/main.py```
 * Connect with postgres with the following URI ```postgresql://postgres:postgres@localhost:5430/wine```
-
+** The easiest way to run docker is a linux environment (preferably ubuntu) 
 ## The endpoints you have to create are the following:
-# Part #1
+
+# Part 1
     1. '/api/getMaxFixedAcidicity'
     2. '/api/getNeutralWines' -> neutral is the ph range between 6-8
     3. '/api/getTopAlcoholWines' -> 10 most alcoholic wines
     4. '/api/getBottomDensityWines -> 10 least density wines
 # Part 2
+
     1. '/api/addWine' -> post, insert one or more new wines
     2. '/api/editWine' -> post, update an existing wine
     3. '/api/removeWine' -> post, remove a wine
+
 # Hints:
    You DON'T have to create any table or database. Just run and connect to the database given, create the models, recources and schemas needed for the exercise
    
---
+++ exercises/exercises.py
@@ -0,0 +1,16 @@
+abbr = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+states = {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
+
+# ************************************************* 1 *************************************************
+# create a list with the full name of every state
+
+# ************************************************* 2 *************************************************
+# create a list with the lower case of the states abbreviations
+
+# ************************************************* 3 *************************************************
+# create a list with the full names of the states that their name ends with 'a'
+
+# ************************************************* 4 *************************************************
+# create a list of dictionaries for every state eg. [{"AL":"Alabama"}, {"AK":"Alaska"}, ...]
+
+### NOTE: USE ONLY LIST COPREHENSIONS OR LAMBDA FUNCTIONS!!!
